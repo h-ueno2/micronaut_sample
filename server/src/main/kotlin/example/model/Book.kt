2e41287440768b41b13dca52de883ef8fb6dbcb3
h-ueno2 <menbou1212@gmail.com>
2020-12-13 12:48:05 +0900

    出版日の形式修正

--- server/src/main/kotlin/example/model/Book.kt
+++ server/src/main/kotlin/example/model/Book.kt
@@ -1,5 +1,6 @@
 package example.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.micronaut.core.annotation.Introspected
 import java.sql.Date
@@ -19,6 +20,7 @@ data class Book(
         @JsonProperty("publisher")
         var publisher: String?,
 
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         @JsonProperty("publication_date")
         var publicationDate: Date?,
 )
